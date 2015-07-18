5da054a837efc70787d478223d166cb8a1addfb4
Ivan Kozik <ivan@ludios.org>
2015-07-18 11:18:50 +0000

    Report concurrency level

--- libgrabsite/wpull_hooks.py
+++ libgrabsite/wpull_hooks.py
@@ -169,6 +169,7 @@ jobData = {
 	"url": open(os.path.join(workingDir, "start_url")).read().strip(),
 	"started_at": os.stat(os.path.join(workingDir, "start_url")).st_mtime,
 	"suppress_ignore_reports": True,
+	"concurrency": 1,
 	"bytes_downloaded": 0,
 	"items_queued": 0,
 	"items_downloaded": 0,
