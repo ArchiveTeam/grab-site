2222aafa74da602f6b36052b3600f9677ea9e61e
Ivan Kozik <ivan@ludios.org>
2015-07-18 11:00:08 +0000

    Spaces -> tabs

--- libgrabsite/dupes.py
+++ libgrabsite/dupes.py
@@ -1,49 +1,49 @@
-# encoding=utf-8
-'''Duplicate page content database.'''
-
+"""
+Duplicate page content database.
+"""
 
 class DupesOnDisk(object):
-    def __init__(self, filename):
-        import lmdb
-        # TODO: lmdb needs a sparse file; fail early instead of using 1TB
-        # of disk on filesystems with no sparse file support
-        for map_size in (1024*1024*1024*1024, 2**31-1):
-            try:
-                self._env = lmdb.open(
-                    filename,
-                    writemap=True,
-                    sync=False,
-                    metasync=False,
-                    # http://lmdb.readthedocs.org/en/release/#lmdb.Environment
-                    map_size=map_size)
-            except OverflowError:
-                pass
-            else:
-                break
+	def __init__(self, filename):
+		import lmdb
+		# TODO: lmdb needs a sparse file; fail early instead of using 1TB
+		# of disk on filesystems with no sparse file support
+		for map_size in (1024*1024*1024*1024, 2**31-1):
+			try:
+				self._env = lmdb.open(
+					filename,
+					writemap=True,
+					sync=False,
+					metasync=False,
+					# http://lmdb.readthedocs.org/en/release/#lmdb.Environment
+					map_size=map_size)
+			except OverflowError:
+				pass
+			else:
+				break
 
-    def get_old_url(self, digest):
-        with self._env.begin() as txn:
-            maybe_url = txn.get(digest)
-            if maybe_url is None:
-                return maybe_url
-            return maybe_url.decode('utf-8')
+	def get_old_url(self, digest):
+		with self._env.begin() as txn:
+			maybe_url = txn.get(digest)
+			if maybe_url is None:
+				return maybe_url
+			return maybe_url.decode('utf-8')
 
-    def set_old_url(self, digest, url):
-        with self._env.begin(write=True) as txn:
-            return txn.put(digest, url.encode("utf-8"))
+	def set_old_url(self, digest, url):
+		with self._env.begin(write=True) as txn:
+			return txn.put(digest, url.encode("utf-8"))
 
 
 class DupesInMemory(object):
-    def __init__(self):
-        self._digests = {}
+	def __init__(self):
+		self._digests = {}
 
-    def get_old_url(self, digest):
-        return self._digests.get(digest)
+	def get_old_url(self, digest):
+		return self._digests.get(digest)
 
-    def set_old_url(self, digest, url):
-        self._digests[digest] = url
+	def set_old_url(self, digest, url):
+		self._digests[digest] = url
 
 
 __all__ = [
-    'DupesOnDisk', 'DupesInMemory'
+	'DupesOnDisk', 'DupesInMemory'
 ]
--- libgrabsite/plugin.py
+++ libgrabsite/plugin.py
@@ -12,55 +12,55 @@ from libgrabsite.dupes import DupesInMemory, DupesOnDisk
 
 
 class NoFsyncSQLTable(SQLiteURLTable):
-    @classmethod
-    def _apply_pragmas_callback(cls, connection, record):
-        super()._apply_pragmas_callback(connection, record)
-        connection.execute('PRAGMA synchronous=OFF')
+	@classmethod
+	def _apply_pragmas_callback(cls, connection, record):
+		super()._apply_pragmas_callback(connection, record)
+		connection.execute('PRAGMA synchronous=OFF')
 
 
 class DupSpottingProcessingRule(wpull.processor.rule.ProcessingRule):
-    def __init__(self, *args, **kwargs):
-        self.dupes_db = kwargs.pop('dupes_db', None)
-        super().__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		self.dupes_db = kwargs.pop('dupes_db', None)
+		super().__init__(*args, **kwargs)
 
-    def scrape_document(self, request, response, url_item):
-        if response.body.size() < 30*1024*1024:
-            dupes_db = self.dupes_db
-            body = response.body.content()
-            if HTMLReader.is_response(response):
-                body = dupespotter.process_body(body, response.request.url)
-            digest = hashlib.md5(body).digest()
-            if dupes_db is not None:
-                dupe_of = dupes_db.get_old_url(digest)
-            else:
-                dupe_of = None
-            if dupe_of is not None:
-                # Don't extract links from pages we've already seen
-                # to avoid loops that descend a directory endlessly
-                print("  DUPE {}\n      OF {}".format(response.request.url, dupe_of))
-                return
-            else:
-                if dupes_db is not None:
-                    dupes_db.set_old_url(digest, response.request.url)
+	def scrape_document(self, request, response, url_item):
+		if response.body.size() < 30*1024*1024:
+			dupes_db = self.dupes_db
+			body = response.body.content()
+			if HTMLReader.is_response(response):
+				body = dupespotter.process_body(body, response.request.url)
+			digest = hashlib.md5(body).digest()
+			if dupes_db is not None:
+				dupe_of = dupes_db.get_old_url(digest)
+			else:
+				dupe_of = None
+			if dupe_of is not None:
+				# Don't extract links from pages we've already seen
+				# to avoid loops that descend a directory endlessly
+				print("  DUPE {}\n      OF {}".format(response.request.url, dupe_of))
+				return
+			else:
+				if dupes_db is not None:
+					dupes_db.set_old_url(digest, response.request.url)
 
-        super().scrape_document(request, response, url_item)
+		super().scrape_document(request, response, url_item)
 
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
-    '--dupes-db',
-    metavar='DIR',
-    default=':memory:',
-    help='save dupes db into DIR instead of memory',
+	'--dupes-db',
+	metavar='DIR',
+	default=':memory:',
+	help='save dupes db into DIR instead of memory',
 )
 args = arg_parser.parse_args(wpull_plugin.plugin_args.split())
 
 if args.dupes_db == ':memory:':
-    dupes_db = DupesInMemory()
+	dupes_db = DupesInMemory()
 else:
-    dupes_db = DupesOnDisk(args.dupes_db)
+	dupes_db = DupesOnDisk(args.dupes_db)
 
 wpull_plugin.factory.class_map['URLTableImplementation'] = NoFsyncSQLTable
 wpull_plugin.factory.class_map['ProcessingRule'] = functools.partial(
-    DupSpottingProcessingRule, dupes_db=dupes_db
+	DupSpottingProcessingRule, dupes_db=dupes_db
 )
