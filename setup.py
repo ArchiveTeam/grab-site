4487c43c8359f222692e199c2f41d0d86a1cfd36
Ivan Kozik <ivan@ludios.org>
2015-10-21 22:38:47 +0000

    Use wpull>=1.2.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
 	package_data={"libgrabsite": ["*.html", "ignore_sets/*"]},
 	install_requires=[
 		"click>=4.1",
-		"wpull>=1.2.1",
+		"wpull>=1.2.2",
 		"cchardet>=0.3.5",
 		"manhole>=1.0.0",
 		"lmdb>=0.86",
