f275b8bc5cb5fadcb5e2205b393b27fd1d2399f6
Ivan Kozik <ivan@ludios.org>
2016-01-15 07:08:14 +0000

    Implement the lmdb stress test

--- libgrabsite/stress_test_lmdb.py
+++ libgrabsite/stress_test_lmdb.py
@@ -1,6 +1,9 @@
+import os
 import sys
 import json
+import hashlib
 import random
+import tempfile
 from libgrabsite.dupes import DupesOnDisk
 
 def make_heap_object():
@@ -23,9 +26,32 @@ def do_work_on_heap(obj):
 		assert num == expect, (num, expect)
 		expect += 1
 
+d = DupesOnDisk(tempfile.NamedTemporaryFile().name)
+
+def get_random_digest():
+	# TODO: optimize?
+	rand = os.urandom(16)
+	return hashlib.md5(rand).digest()
+
+def get_fake_url():
+	if random.random() < 0.1:
+		fake_url = " " * random.randint(1, 10000)
+	else:
+		fake_url = " " * random.randint(1, 1000)
+	return fake_url
+
+def do_possibly_corrupting_work():
+	for i in range(random.randint(1, 10000)):
+		d.get_old_url(get_random_digest())
+		key = get_random_digest()
+		url = get_fake_url()
+		d.set_old_url(key, url)
+		assert d.get_old_url(key) == url
+
 def main():
 	while True:
 		obj = make_heap_object()
+		do_possibly_corrupting_work()
 		do_work_on_heap(obj)
 		print(".", end=" ")
 		sys.stdout.flush()
