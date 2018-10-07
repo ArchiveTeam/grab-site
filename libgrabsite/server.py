f24959ce4a8490998147d145cd301d224dfd4d94
Ivan Kozik <ivan@ludios.org>
2018-10-07 21:38:47 +0000

    gs-server: alignment; use f"" strings; use camel_case for non-autobahn methods

--- libgrabsite/server.py
+++ libgrabsite/server.py
@@ -13,75 +13,74 @@ class GrabberServerProtocol(WebSocketServerProtocol):
 
 	def onConnect(self, request):
 		self.peer = request.peer
-		print("{} connected".format(self.peer))
+		print(f"{self.peer} connected")
 		self.factory.clients.add(self)
 
 	def onClose(self, wasClean, code, reason):
-		print("{} disconnected".format(self.peer))
+		print(f"{self.peer} disconnected")
 		self.factory.clients.discard(self)
 
-	def broadcastToDashboards(self, obj):
+	def broadcast_to_dashboards(self, obj):
 		for client in self.factory.clients:
 			if client.mode == "dashboard":
-				##print("Broadcasting", pprint.pformat(obj))
 				client.sendMessage(json.dumps(obj).encode("utf-8"))
 
 	def onMessage(self, payload, isBinary):
-		##print(payload)
-		obj = json.loads(payload.decode("utf-8"))
+		obj  = json.loads(payload.decode("utf-8"))
 		type = obj["type"]
 		if self.mode is None and type == "hello" and obj.get("mode"):
 			mode = obj["mode"]
 			if mode in ("dashboard", "grabber"):
 				self.mode = mode
 				if mode == "grabber":
-					print("{} is grabbing {}".format(self.peer, obj["url"]))
+					print(f'{self.peer} is grabbing {obj["url"]}')
 				elif mode == "dashboard":
-					print("{} is dashboarding with {}".format(self.peer, obj.get("user_agent", "(no User-Agent)")))
+					user_agent = obj.get("user_agent", "(no User-Agent)")
+					print(f"{self.peer} is dashboarding with {user_agent}")
 		elif self.mode == "grabber":
 			if type == "download":
-				self.broadcastToDashboards({
-					"type": type,
-					"job_data": obj["job_data"],
-					"url": obj["url"],
+				self.broadcast_to_dashboards({
+					"type":          type,
+					"job_data":      obj["job_data"],
+					"url":           obj["url"],
 					"response_code": obj["response_code"],
-					"wget_code": obj["response_message"]
+					"wget_code":     obj["response_message"]
 				})
 			elif type in ("stdout", "stderr"):
-				self.broadcastToDashboards({
-					"type": type,
+				self.broadcast_to_dashboards({
+					"type":     type,
 					"job_data": obj["job_data"],
-					"message": obj["message"]
+					"message":  obj["message"]
 				})
 			elif type == "ignore":
-				self.broadcastToDashboards({
-					"type": type,
+				self.broadcast_to_dashboards({
+					"type":     type,
 					"job_data": obj["job_data"],
-					"url": obj["url"],
-					"pattern": obj["pattern"],
+					"url":      obj["url"],
+					"pattern":  obj["pattern"],
 				})
 
 	# Called when we get an HTTP request instead of a WebSocket request
 	def sendServerStatus(self, redirectUrl=None, redirectAfter=0):
 		requestPath = self.http_request_uri.split("?")[0]
 		if requestPath == "/":
-			self.sendPage("dashboard.html", 200, "OK", "text/html; charset=UTF-8")
+			self.send_page("dashboard.html", 200, "OK", "text/html; charset=UTF-8")
 		elif requestPath == "/favicon.ico":
-			self.sendPage("favicon.ico", 200, "OK", "image/x-icon")
+			self.send_page("favicon.ico", 200, "OK", "image/x-icon")
 		else:
-			self.sendPage("404.html", 404, "Not Found", "text/html; charset=UTF-8")
+			self.send_page("404.html", 404, "Not Found", "text/html; charset=UTF-8")
 
 	# Based on AutoBahn's WebSocketServerProtocol.sendHtml
-	def sendPage(self, fname, code, status, contentType):
+	def send_page(self, fname, code, status, content_type):
 		with open(os.path.join(os.path.dirname(__file__), fname), "rb") as f:
-			responseBody = f.read()
-		response = "HTTP/1.1 {} {}\r\n".format(code, status)
-		response += "Content-Type: {}\r\n".format(contentType)
-		response += "Content-Length: {}\r\n".format(len(responseBody))
+			response_body = f.read()
+		response =  f"HTTP/1.1 {code} {status}\r\n"
+		response += f"Content-Type: {content_type}\r\n"
+		response += f"Content-Length: {len(response_body)}\r\n"
 		response += "X-Frame-Options: DENY\r\n"
 		response += "\r\n"
 		self.sendData(response.encode("utf-8"))
-		self.sendData(responseBody)
+		self.sendData(response_body)
 
 
 class GrabberServerFactory(WebSocketServerFactory):
@@ -100,7 +99,7 @@ def main():
 	for port in ports:
 		coro = loop.create_server(factory, interface, port)
 		loop.run_until_complete(coro)
-		print("grab-site server listening on {}:{}".format(interface, port))
+		print(f"grab-site server listening on {interface}:{port}")
 
 	loop.run_forever()
 
