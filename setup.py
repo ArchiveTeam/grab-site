737314070951ef44792027f8592bfc8524721613
Ivan Kozik <ivan@ludios.org>
2018-10-07 22:26:59 +0000

    Use ludios/wpull 3.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import libgrabsite
 
 install_requires = [
 	"click>=6.3",
-	"wpull==3.0.2",
+	"wpull==3.0.3",
 	"manhole>=1.0.0",
 	"lmdb>=0.89",
 	"autobahn>=0.12.1",
@@ -41,7 +41,7 @@ setup(
 	package_data={"libgrabsite": ["*.html", "*.ico", "*.txt", "ignore_sets/*"]},
 	install_requires=install_requires,
 	dependency_links=[
-		"https://github.com/ludios/wpull/tarball/master#egg=wpull-3.0.2",
+		"https://github.com/ludios/wpull/tarball/master#egg=wpull-3.0.3",
 		"https://github.com/ludios/pyre2/tarball/master#egg=fb-re2-1.0.7"
 	]
 )
