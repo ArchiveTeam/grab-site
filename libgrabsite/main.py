fd2b403a3e2c56134a379d628afa701edb4c9f68
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-06-23 18:52:49 -0400

    clearer error when URL is invalid
    I thought it was something wrong with my regex ignoreset until I re-read the command I had run!

--- libgrabsite/main.py
+++ libgrabsite/main.py
@@ -207,7 +207,10 @@ which_wpull_args_partial, which_wpull_command):
 	if not id:
 		id = binascii.hexlify(os.urandom(16)).decode('utf-8')
 	ymd                  = datetime.datetime.utcnow().isoformat()[:10]
-	no_proto_no_trailing = claim_start_url.split('://', 1)[1].rstrip('/')[:100]
+	try:
+	    no_proto_no_trailing = claim_start_url.split('://', 1)[1].rstrip('/')[:100]
+	except IndexError:
+		sys.exit("Invalid URL!")
 	unwanted_chars_re    = r'[^-_a-zA-Z0-9%\.,;@+=]'
 	warc_name            = "{}-{}-{}".format(re.sub(unwanted_chars_re, '-', no_proto_no_trailing).lstrip('-'), ymd, id[:8])
 
